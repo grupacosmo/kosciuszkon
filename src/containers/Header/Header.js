c2d686f8c90540f7149276cbda09d423d4cdc9a6
itspatys <patryk.pierzchaa@gmail.com>
2023-02-26 18:43:22 +0100

    Header change

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -9,7 +9,7 @@ const MenuItems = ({setIsMenuOpen}) => {
         <>
             <li><a onClick={handleClick} href="#o wydarzeniu">O wydarzeniu</a></li>
             <li><a onClick={handleClick} href="#tematy">Tematy turnieju</a></li>
-            {/* <li><a onClick={handleClick} href="#harmonogram">Harmonogram</a></li> */}
+            <li><a onClick={handleClick} href="#harmonogram">Harmonogram</a></li>
             <li><a onClick={handleClick} href="#zapisy">Zapisy</a></li>
             <li><a onClick={handleClick} href="#partnerzy">Partnerzy</a></li>
             <li><a onClick={handleClick} href="#kontakt">Kontakt</a></li>
